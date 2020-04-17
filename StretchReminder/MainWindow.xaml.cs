bd4b7d5e657cbfa6aba4eb8f4f035e252a5f9094
unknown <peter.ozbot@gmail.com>
2020-04-17 19:01:00 +0200

    scalfolding the project

---
+++ StretchReminder/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+namespace StretchReminder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ShowNotification("balalbal", 10);
+        }
+
+        public void ShowNotification(string description, double amount)
+        {
+            string xml = $@"<toast>
+                      <visual>
+                        <binding template='ToastGeneric'>
+                          <text>Expense added</text>
+                          <text>Description: {description} - Amount: {amount} </text>
+                        </binding>
+                      </visual>
+                    </toast>";
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xml);
+
+            ToastNotification toast = new ToastNotification(doc);
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+        }
+    }
+}
