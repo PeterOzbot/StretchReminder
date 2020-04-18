e380e8948368376eaa9ae66886c8eebaa297aae9
unknown <peter.ozbot@gmail.com>
2020-04-18 18:23:49 +0200

    Implemented tray minimization and toast notification concept

--- StretchReminder/MainWindow.xaml.cs
+++ StretchReminder/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,14 +24,48 @@ namespace StretchReminder
     /// </summary>
     public partial class MainWindow : Window
     {
+        private System.Windows.Forms.NotifyIcon m_notifyIcon;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // initialise code here
+            m_notifyIcon = new System.Windows.Forms.NotifyIcon();
+            m_notifyIcon.BalloonTipText = "The app has been minimised. Click the tray icon to show.";
+            m_notifyIcon.BalloonTipTitle = "The App";
+            m_notifyIcon.Text = "The App";
+            m_notifyIcon.Icon = new Icon(Application.GetResourceStream(new Uri("pack://application:,,,/StretchReminderIcon.ico")).Stream); 
+            m_notifyIcon.Click += new EventHandler(m_notifyIcon_Click);
         }
+
+
+        TimeSpan _duration = new TimeSpan(0, 0, 10);
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            ShowNotification("balalbal", 10);
+            //ShowNotification("balalbal", 10);
+            DateTime startTime = DateTime.Now;
+            System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+
+
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            // Do Stuff
+            _duration = _duration.Subtract(new TimeSpan(0, 0, 0, 1));
+            if (_duration.Seconds == 0)
+            {
+
+                return;
+            }
+
+            // Updating the Label which displays the current second
+            Label.Content = _duration.ToString("mm':'ss");
         }
 
         public void ShowNotification(string description, double amount)
@@ -49,5 +85,47 @@ namespace StretchReminder
             ToastNotification toast = new ToastNotification(doc);
             ToastNotificationManager.CreateToastNotifier().Show(toast);
         }
+
+        /////////////////////////////////////////////////////////////
+        void OnClose(object sender, CancelEventArgs args)
+        {
+            m_notifyIcon.Dispose();
+            m_notifyIcon = null;
+        }
+
+        private WindowState m_storedWindowState = WindowState.Normal;
+        void OnStateChanged(object sender, EventArgs args)
+        {
+            if (WindowState == WindowState.Minimized)
+            {
+                Hide();
+                if (m_notifyIcon != null)
+                    m_notifyIcon.ShowBalloonTip(2000);
+            }
+            else
+                m_storedWindowState = WindowState;
+        }
+        void OnIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs args)
+        {
+            CheckTrayIcon();
+        }
+
+        void m_notifyIcon_Click(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = m_storedWindowState;
+        }
+        void CheckTrayIcon()
+        {
+            ShowTrayIcon(!IsVisible);
+        }
+
+        void ShowTrayIcon(bool show)
+        {
+            if (m_notifyIcon != null)
+                m_notifyIcon.Visible = show;
+        }
+
+
     }
 }
